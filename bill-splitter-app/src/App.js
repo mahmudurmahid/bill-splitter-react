e89ad20287febc08f549a26c67947dece9d82044
mahmudurmahid <m.mahid@icloud.com>
2025-07-07 15:30:52 +0100

    friend component with image, name, and balance shown on sidebar

--- bill-splitter-app/src/App.js
+++ bill-splitter-app/src/App.js
@@ -35,8 +35,31 @@ function FriendsList() {
   return (
     <ul>
       {friends.map((friend) => (
-        <li>{friend.name}</li>
+        <Friend friend={friend} key={friend.id} />
       ))}
     </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name}></img>
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <button className="button">Select</button>
+    </li>
   );
 }
