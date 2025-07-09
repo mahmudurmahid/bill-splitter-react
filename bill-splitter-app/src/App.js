29c34f5131e2b9aee5d09afe0f8e79804f7a01ed
mahmudurmahid <m.mahid@icloud.com>
2025-07-09 10:49:10 +0100

    can add new friend on form

--- bill-splitter-app/src/App.js
+++ bill-splitter-app/src/App.js
@@ -87,13 +87,43 @@ function Friend({ friend }) {
 }
 
 function AddFriendForm() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}=${id}`,
+      balance: 0,
+    };
+
+    console.log(newFriend);
+
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>Friend Name 👤</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <label>Image URL 📸</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
 
       <Button>Add</Button>
     </form>
