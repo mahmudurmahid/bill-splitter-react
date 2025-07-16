65ea465f7e6e3976be3f26b88ce1f1430fda88a9
mahmudurmahid <m.mahid@icloud.com>
2025-07-16 11:36:29 +0100

    reset split bill form inputs (ie bill value, your expense, who paid) to default when clicking on new friend if values are filled in

--- bill-splitter-app/src/App.js
+++ bill-splitter-app/src/App.js
@@ -80,6 +80,7 @@ export default function App() {
         <SplitBillForm
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
